d130d42c708900c10294017f5f49dab3d7f69d0b
LKaMe <15937515378@163.com>
2020-02-10 19:16:55 +0800

    2

---
+++ code/表情包/search.py
@@ -0,0 +1,29 @@
+#-*- coding:UTF-8 -*-
+import glob
+import time
+
+import itchat
+from itchat.content import TEXT, PICTURE
+
+
+imgs = []
+
+def searchImage(text):
+    print('收到关键词: ', text)
+    for name in glob.glob('F:/python/表情包/*'+text+'*.jpg'):
+        imgs.append(name)
+
+
+@itchat.msg_register([PICTURE, TEXT])
+def text_reply(msg):
+    searchImage(msg.text)
+    for img in imgs[:6]:
+        msg.user.send_image(img)
+        time.sleep(0.3)
+        print('开始发送表情： ', img)
+    imgs.clear()
+
+
+# itchat.auto_login(hotReload=True)
+itchat.auto_login()
+itchat.run()
