49dfe016f6d67d27a9355f45c4213a5d6dd40980
unknown <15937515378@163.com>
2020-03-05 14:55:49 +0800

    准备GloVe词嵌入矩阵

---
+++ code/python深度学习/第6章/6-11准备GloVe词嵌入矩阵.py
@@ -0,0 +1,8 @@
+# embedding_dim = 100
+
+# embedding_matrix = np.zeros((max_words,embedding_dim))
+# for word,i in word_index.items():
+#     if i < max_words:
+#         embedding_vector = embedding_index.get(word)
+#         if embedding_vector is not None:
+#             embedding_matrix[i] = embedding_vector#嵌入索引(embedding_index)中找不到的词，其嵌入向量全为
