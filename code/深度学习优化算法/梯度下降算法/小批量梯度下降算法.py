1bdd210a149743eaa18534cb456ca5f5060d701c
LKaMe <15937515378@163.com>
2020-05-20 10:56:38 +0800

    新增小批量梯度下降算法

---
+++ code/深度学习优化算法/梯度下降算法/小批量梯度下降算法.py
@@ -0,0 +1,5 @@
+for i in range(nb_epochs):
+    np.random.shuffle(data)
+    for batch in get_batchs(data,batch_size = 50):
+        params_grad = evaluate_gradient(loss_function,batch,params)
+        params = params - learning_rate * params_grad 
