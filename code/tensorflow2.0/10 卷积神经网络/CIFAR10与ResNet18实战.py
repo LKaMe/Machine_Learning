08b6a959e3699b70bac9efb69a53f3a938935986
LKaMe <15937515378@163.com>
2020-07-06 14:13:39 +0800

    修改

--- code/tensorflow2.0/10 卷积神经网络/CIFAR10与ResNet18实战.py
+++ code/tensorflow2.0/10 卷积神经网络/CIFAR10与ResNet18实战.py
@@ -82,4 +82,51 @@ class BasicBlock(layers.Layer):
             x = self.fc(x)
 
             return x 
+        
+        def resnet18():
+            #通过调整模块内部BasicBlock的数量和配置实现不同的ResNet
+            return ResNet([2,2,2,2])
+
+        def resnet34():
+            #通过调整模块内部BasicBlock的数量和配置实现不同的ResNet 
+            return ResNet([3,4,6,3])
+
+        #CIFAR10数据集加载工作
+        (x,y),(x_test,y_test) = datasets.cifar10.load_data()#加载数据集
+        y = tf.squeeze(y,axis = 1)#删除不必要的维度
+        y_test = tf.squeeze(y_test,axis = 1)#删除不必要的维度
+        print(x.shape,y.shape,x_test.shape,y_test.shape)
+
+        train_db = tf.data.Dataset.from_tensor_slices((x,y))#构建训练集
+        #随机打散，预处理，批量化
+        train_db = train_db.shuffle(1000).map(preprocess).batch(512)
+
+        test_db = tf.data.Dataset.from_tensor_slices((x_test,y_test))#构建测试集
+        #随机打散，预处理，批量化
+        test_db = test_db.map(preprocess).batch(512)
+        #采样一个样本
+        sample = next(iter(train_db))
+        print('sample:',sample[0].shape,sample[1].shape,tf.reduce_min(sample[0]),tf.reduce_max(sample[0]))
+
+        def preprocess(x,y):
+            #将数据映射到-1~1
+            x = 2 * tf.cast(x,dtype = tf.float32) / 255. - 1
+            y = tf.cast(y,dtype = tf.int32)#类型转换
+            return x,y 
+
+            for epoch in range(50):#训练epoch 
+                for step,(x,y) in enumerate(train_db):
+                    with tf.GradientTape() as tape :
+                        #[b,32,32,3] => [b,10],前向传播
+                        logits = model(x)
+                        #[b] => [b,10],one-ht编码
+                        y_onehot = tf.one_hot(y,depth = 10)
+                        #计算交叉熵
+                        loss = tf.losses.categorical_crossentropy(y_onehot,logits,from_logits = True)
+                        loss = tf.reduce_mean(loss)
+                    #计算梯度信息
+                    grads = tape.gradient(loss,model.trainable_variables)
+                    #更新网络参数
+                    optimizer.apply_gradients(zip(grads,model.trainable_varoables))
+                    
 
